383c7a5735a203d21970fc42d402c7096f9eb1d4
Mattia Procopio <promat85@gmail.com>
2022-05-26 19:54:01 +0200

    Add file descriptor set binary file to build

--- compiled/rust/lightspeed/build.rs
+++ compiled/rust/lightspeed/build.rs
@@ -2,10 +2,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .out_dir("src/")
         .build_client(false)
-        .compile(&["../../../protocol/server.proto"], &["../../../"])?;
-    tonic_build::configure()
-        .out_dir("src/")
-        .build_client(false)
-        .compile(&["../../../protocol/devices/device.proto"], &["../../../"])?;
+	.file_descriptor_set_path("src/lightspeed.bin")
+        .compile(
+	    &["../../../protocol/server.proto", "../../../protocol/devices/device.proto"], &["../../../"],
+	)?;
     Ok(())
 }
