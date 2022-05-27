4615e456adeb1c2c0e3bb8c6d013445808287568
Mattia Procopio <promat85@gmail.com>
2022-05-27 16:42:07 +0200

    Use relative path to cargo config

--- compiled/rust/lightspeed/build.rs
+++ compiled/rust/lightspeed/build.rs
@@ -1,14 +1,23 @@
+use std::env;
+use std::fs;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let proto_include_path = fs::canonicalize(env::var("PROTO_ROOT").unwrap()).unwrap();
+    println!("PROTO_ROOT: {}", proto_include_path.display());
+
     tonic_build::configure()
         .out_dir("src/")
         .build_client(false)
         .file_descriptor_set_path("src/lightspeed.bin")
         .compile(
             &[
-                "../../../protocol/server.proto",
-                "../../../protocol/devices/device.proto",
+                format!("{}/protocol/server.proto", proto_include_path.display()),
+                format!(
+                    "{}/protocol/devices/device.proto",
+                    proto_include_path.display()
+                ),
             ],
-            &["../../../"],
+            &[proto_include_path],
         )?;
     Ok(())
 }
